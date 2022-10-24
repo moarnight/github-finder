6a7ec0e163780f41cf440e71d19c43023cf4d830
moarnight <moarnight@gmail.com>
2022-10-24 18:07:16 +0200

    Switch from useState to useReducer within github context

---
+++ src/context/github/GithubReducers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
