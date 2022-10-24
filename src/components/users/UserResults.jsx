3df09c03b89dbf6c6ac42ed8eb2c5eefd76b96ad
moarnight <moarnight@gmail.com>
2022-10-24 18:14:03 +0200

    Clean up fetch users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
   const { users, loading, fetchUsers } = useContext(GithubContext);
-  useEffect(() => {
-    fetchUsers();
-  }, []);
 
   if (!loading) {
     return (
