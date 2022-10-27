4bf750c04a73e19da4bb6ac9c3fa6defa36ff308
moarnight <moarnight@gmail.com>
2022-10-27 12:29:46 +0200

    Get single user

--- src/context/github/GithubReducers.js
+++ src/context/github/GithubReducers.js
@@ -8,6 +8,13 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
+    case 'GET_USER': {
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    }
     case 'SET_LOADING':
       return {
         ...state,
